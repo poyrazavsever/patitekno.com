e2c09bcbc48abb4ccde6516b7fe372aab1cf0ce2
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-06 10:49:34 +0300

    feat: created eduCard component

---
+++ components/shared/eduCard.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const EduCard = () => {
+
+  /* Deneme İçin*/
+  const eduList = [
+    {
+      name: "HTML 101",
+      iconName : "html",
+      link: ""
+    },
+    {
+      name: "CSS 101",
+      iconName : "css",
+      link: ""
+    },
+    {
+      name: "Javascript 101",
+      iconName : "js",
+      link: ""
+    },
+    
+    {
+      name: "Typescript 101",
+      iconName : "ts",
+      link: ""
+    }
+  ]
+
+  return (
+    <div className='flex flex-wrap items-center gap-8'>
+      {
+        eduList?.map((edu) => (
+          <div className='w-fit flex flex-col items-center gap-4 p-8 rounded-md border border-neutral-300'>
+            <img src={`https://skillicons.dev/icons?i=${edu.iconName}`} alt="edu logo for card" className='w-32 h-32'/>
+            <span className='font-medium text-textColor'>{edu.name}</span>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default EduCard
