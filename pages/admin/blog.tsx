53606291c1a72462b0b2dd3b381a107e1aef182a
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-07 23:56:48 +0300

    feat: added tab future to blog admin page

--- pages/admin/blog.tsx
+++ pages/admin/blog.tsx
@@ -1,6 +1,7 @@
-import { ReactElement } from "react"
+import { ReactElement, useState } from "react"
 import { NextPageWithLayout } from "../_app"
 import AdminLayout from "@/components/layout/AdminLayout"
+import classNames from "classnames";
 
 import { MdEdit, MdDelete, MdLink } from "react-icons/md";
 
@@ -32,33 +33,74 @@ const blogList: BlogItem[] = [
     },
 ];
 
+const links = [
+    {
+        id: 0,
+        name: "Blog Yazıları"
+    },
+    {
+        id: 1,
+        name: "Blog Yazısı Ekle"
+    },
+    {
+        id: 2,
+        name: "Kategoriler"
+    },
+    {
+        id: 3,
+        name: "Kategori Ekle"
+    }
+]
 
 const AdminBlog : NextPageWithLayout = () => {
 
+    const [currentTab, setCurrentTab] = useState(0)
+
+
     return (
         <div>
-            <h1 className="text-2xl font-semibold text-primary">Blog Yazıları</h1>
-
-            <div className="flex flex-wrap gap-4 mt-8">
+            <nav className="flex items-center gap-4 pb-6 text-textColor font-medium">
                 {
-                    blogList?.map((item, index) => (
-                        <div key={index} className="w-72 rounded-md border border-neutral-200">
-                            {item.imageUrl && (
-                                <img src={item.imageUrl} alt={item.title} className="w-full h-40 object-cover rounded-t-md"/>
-                            )}
-                            <div className="flex flex-col items-end gap-2 p-4">
-                                <h3 className="w-full text-start text-xl font-semibold mb-2 text-primary">{item.title}</h3>
-                                <div className="text-xl flex items-center gap-4">  
-                                    <button><MdEdit className="text-sky-400"/></button>
-                                    <button><MdDelete className="text-red-400"/></button>
-                                    <a href="#"><MdLink className="text-neutral-400 -rotate-45"/></a>
-                                </div>
-                            </div>
-                        </div>
+                    links.map((link) => (
+                        <button key={link.id} onClick={() => setCurrentTab(link.id)} className={classNames(
+                            "py-1 px-3 rounded-sm cursor-pointer transition-all",
+                            {
+                                "bg-primary text-background": currentTab == link.id
+                            }
+                        )}>{link.name}</button>
                     ))
                 }
-            </div>
+            </nav>
 
+            {
+                currentTab === 0 ? (
+                    <>
+                        <h1 className="text-2xl font-semibold text-primary mt-8">Blog Yazıları</h1>
+
+                        <div className="flex flex-wrap gap-4 mt-8">
+                            {
+                                blogList?.map((item, index) => (
+                                    <div key={index} className="w-72 rounded-md border border-neutral-200">
+                                        {item.imageUrl && (
+                                            <img src={item.imageUrl} alt={item.title} className="w-full h-40 object-cover rounded-t-md"/>
+                                        )}
+                                        <div className="flex flex-col items-end gap-2 p-4">
+                                            <h3 className="w-full text-start text-xl font-semibold mb-2 text-primary">{item.title}</h3>
+                                            <div className="text-xl flex items-center gap-4">  
+                                                <button><MdEdit className="text-sky-400"/></button>
+                                                <button><MdDelete className="text-red-400"/></button>
+                                                <a href="#"><MdLink className="text-neutral-400 -rotate-45"/></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </>
+                ) : ""
+            }
+
+            
         </div>
     )
 }
