93eed79d1e5536ed96cfa44d10b315917202592a
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-08 10:42:58 +0300

    feat: added tabs to lesson admin page

--- pages/admin/egitim.tsx
+++ pages/admin/egitim.tsx
@@ -1,10 +1,42 @@
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 import { NextPageWithLayout } from '../_app'
 import AdminLayout from '@/components/layout/AdminLayout'
+import classNames from 'classnames'
 
 const AdminLesson : NextPageWithLayout = () => {
+
+  const [currentTab, setCurrentTab] = useState(0)
+
+  const links = [
+    {
+        id: 0,
+        name: "Mevcut Dersler"
+    },
+    {
+        id: 1,
+        name: "Ders Yazısı Ekle"
+    },
+    {
+        id: 2,
+        name: "Ders Ekle"
+    },
+]
+
   return (
-    <div>AdminLesson</div>
+    <div>
+        <nav className="flex items-center gap-4 pb-6 text-textColor font-medium">
+            {
+            links.map((link) => (
+                <button key={link.id} onClick={() => setCurrentTab(link.id)} className={classNames(
+                  "py-1 px-3 rounded-sm cursor-pointer transition-all",
+                  {
+                    "bg-primary text-background": currentTab == link.id
+                  }
+                )}>{link.name}</button>
+            ))
+           }
+        </nav>
+    </div>
   )
 }
 
