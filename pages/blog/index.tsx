ee6ec5a3136388e7bdfba4e3c1679de04e496f9d
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-13 13:45:48 +0300

    feat: done blog detail page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -2,7 +2,35 @@ import React from "react";
 import BlogData from "@/components/shared/blogData";
 
 
+type BlogItem = {
+  title: string;
+  description: string;
+  date: string;
+  imageUrl?: string;
+};
+
 const BlogList = () => {
+
+  const blogList: BlogItem[] = [
+    {
+      title: "HTML 101",
+      description: "HTML'in temellerini ve web'deki rolünü öğrenin.",
+      date: "07 Haziran 2025",
+      imageUrl: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fgaleri8.uludagsozluk.com%2F446%2Fresim-cizmek_698899.jpg&f=1&nofb=1&ipt=0201288d9417837cfb5faf5a31579819dcec485da99642dc6cfc5f301265aa33",
+    },
+    {
+      title: "CSS ile Stil Verme",
+      description: "Web sitenizi güzelleştirmek için CSS'in gücünü keşfedin.",
+      date: "05 Haziran 2025",
+      imageUrl: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fgaleri8.uludagsozluk.com%2F446%2Fresim-cizmek_698899.jpg&f=1&nofb=1&ipt=0201288d9417837cfb5faf5a31579819dcec485da99642dc6cfc5f301265aa33",
+    },
+    {
+      title: "JavaScript'e Giriş",
+      description: "Sayfanıza etkileşim kazandırmak artık çok kolay!",
+      date: "03 Haziran 2025",
+      imageUrl: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fgaleri8.uludagsozluk.com%2F446%2Fresim-cizmek_698899.jpg&f=1&nofb=1&ipt=0201288d9417837cfb5faf5a31579819dcec485da99642dc6cfc5f301265aa33",
+    },
+  ];
 
   return (
     <div className="flex flex-col items-start gap-4 mt-16">
