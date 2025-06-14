94ba85119aefcb52a8248f7948616a24961a6cc2
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-14 15:21:57 +0300

    feat: added dark settings to globascss n document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="">
       <Head />
-      <body className="antialiased">
+      <body className="antialiased dark:bg-backgroundDark bg-background">
         <Main />
         <NextScript />
       </body>
