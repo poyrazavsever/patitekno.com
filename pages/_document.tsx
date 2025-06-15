3821fe206ce516eec66fe55436a7ef7c292e507a
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-15 17:22:51 +0300

    feat: fixed dark mode problem in admin page site's

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="tr" className="dark">
+    <Html lang="tr">
       <Head />
       <body className="antialiased dark:bg-backgroundDark bg-background">
         <Main />
