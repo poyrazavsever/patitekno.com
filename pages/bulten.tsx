cd152d68e8cfe4259f84d9d91277cc34dc2e5b7f
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-15 13:26:27 +0300

    feat: done responsive design bulten page site

--- pages/bulten.tsx
+++ pages/bulten.tsx
@@ -122,8 +122,8 @@ export default function Bulten() {
   return (
     <div className="py-12 space-y-20">
       <div className='flex flex-col items-start gap-4 mt-6 w-full'>
-        <h3 className='text-2xl font-semibold text-primary'>Bültenimize Katıl</h3>
-        <p className='text-textColor mb-2'>Yeni videolar, bloglar ve eğitimlerden ilk sen haberdar ol!</p>
+        <h3 className='text-2xl font-semibold text-primary dark:text-primaryDark'>Bültenimize Katıl</h3>
+        <p className='text-textColor dark:text-textColorDark mb-2'>Yeni videolar, bloglar ve eğitimlerden ilk sen haberdar ol!</p>
 
         <form onSubmit={handleSubmit} className='flex w-full md:w-1/2 gap-2 mt-2'>
           <input
@@ -132,11 +132,11 @@ export default function Bulten() {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="E-posta adresiniz"
-            className='w-36 md:w-fit flex-1 px-4 py-2 border border-neutral-300 rounded-md text-sm text-textColor focus:outline-none focus:ring-2 focus:ring-primary'
+            className='w-36 md:w-fit flex-1 px-4 py-2 border border-neutral-300 dark:border-neutral-600 dark:placeholder:text-gray-400 rounded-md text-sm text-textColor focus:outline-none focus:ring-2 focus:ring-primary dark:focus:ring-primaryDark'
           />
           <button
             onClick={handleSubmit}
-            className='text-xs md:text-base px-4 py-2 bg-primary text-background rounded-md hover:bg-primary/90 transition-colors'> Kayıt Ol </button>
+            className='text-xs md:text-base px-4 py-2 bg-primary dark:bg-primaryDark dark:text-backgroundDark text-background rounded-md hover:bg-primary/90 transition-colors'> Kayıt Ol </button>
         </form>
       </div>
 
